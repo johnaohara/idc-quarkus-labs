62d030639b0a7ef0ee2fa1f73c32162ebbac2a91
John OHara <johara@redhat.com>
2021-09-20 12:09:11 +0100

    Upgrade Spring Boot to 2.5.4, enable AOT compilation and native image generation. Increase MAX jsbc connections to 64

--- spring-todo/src/main/java/com/example/Application.java
+++ spring-todo/src/main/java/com/example/Application.java
@@ -3,11 +3,13 @@ package com.example;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.NativeHint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@NativeHint(options = "--enable-https")
 public class Application {
 
   public static void main(String[] args) {
