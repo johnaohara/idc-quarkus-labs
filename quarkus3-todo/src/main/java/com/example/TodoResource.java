895afefa2c6176a6ef8bc3facf2ca9a5775a55db
John O'Hara <johara@redhat.com>
2023-03-20 18:11:52 +0000

    Add Quarkus3 version

---
+++ quarkus3-todo/src/main/java/com/example/TodoResource.java
@@ -0,0 +1,58 @@
+package com.example;
+
+import io.quarkus.panache.common.Sort;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/api")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class TodoResource {
+
+    @GET
+    public List<Todo> getAll() {
+        return Todo.listAll();
+    }
+
+    @GET
+    @Path("/sorted")
+    public List<Todo> getAllSorted() {
+        return Todo.listAll(Sort.by("order"));
+    }
+
+    @PATCH
+    @Path("/{id}")
+    @Transactional
+    public Response update(Todo todo, @PathParam("id") Long id) {
+        Todo entity = Todo.findById(id);
+        entity.completed = todo.completed;
+        entity.order = todo.order;
+        entity.title = todo.title;
+        entity.url = todo.url;
+        entity.categories = todo.categories;
+        return Response.ok(entity).build();
+    }
+
+    @POST
+    @Transactional
+    public Response createNew(Todo item) {
+        item.persist();
+        return Response.status(Response.Status.CREATED).entity(item).build();
+    }
+
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response delete(@PathParam("id") Long id) {
+        Todo entity = Todo.findById(id);
+        entity.delete();
+        return Response.noContent().build();
+    }
+
+
+}
