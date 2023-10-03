a9675decc3d248a3972a4e5a8e0563feffb42869
Eric Deandrea <edeandrea@redhat.com>
2023-10-03 13:52:56 -0400

    Fix trailing slash so it doesn't matter

--- spring-todo/src/main/java/com/example/TodoController.java
+++ spring-todo/src/main/java/com/example/TodoController.java
@@ -1,16 +1,23 @@
 package com.example;
 
-import lombok.Lombok;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 import javax.transaction.Transactional;
-import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("api")
+@RequestMapping({ "api", "api/" })
 public class TodoController {
 
     private final TodoRepository todoRepository;
