b7b29f755f29c845faaa4d98cc1f3214ed3f3317
Eric Deandrea <edeandrea@redhat.com>
2023-10-12 11:46:37 -0400

    Fix lombok

--- springboot3-reactive-todo/src/main/java/com/example/Todo.java
+++ springboot3-reactive-todo/src/main/java/com/example/Todo.java
@@ -1,8 +1,14 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.xml.bind.annotation.XmlElement;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
@@ -18,32 +24,51 @@ import lombok.ToString;
 
 @Table("Todo")
 @Builder(toBuilder = true)
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class Todo {
 
     @Id
+    @Getter @Setter
     private Long id;
 
     @NotBlank
+    @Getter @Setter
     private String title;
 
+    @Getter @Setter
     private boolean completed;
 
     @Column("ordering")
+    @Getter @Setter
     private int order;
 
+    @Getter @Setter
     private String url;
 
+    @Getter @Setter
     private Long userId;
 
     @Transient
+    @Getter(onMethod_ = @XmlElement) @Setter
     private User user;
 
     @Transient
     private Set<Category> categories;
 
+    @XmlElement
+    public List<Category> getCategories() {
+        return Optional.ofNullable(this.categories)
+          .map(ArrayList::new)
+          .orElseGet(ArrayList::new);
+    }
+
+    public void setCategories(Collection<Category> categories) {
+        this.categories = Optional.ofNullable(this.categories)
+          .orElseGet(LinkedHashSet::new);
+
+        Optional.ofNullable(categories)
+          .ifPresent(this.categories::addAll);
+    }
 }
